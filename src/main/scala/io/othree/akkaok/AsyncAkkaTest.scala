b9f170cbe065ba5ce689ccb0c4f93fca6fd6beab
Andres Blanco <andres.blanco@othree.io>
2018-06-05 08:48:25 -0600

    Fixing codacy issues

--- src/main/scala/io/othree/akkaok/AsyncAkkaTest.scala
+++ src/main/scala/io/othree/akkaok/AsyncAkkaTest.scala
@@ -4,4 +4,4 @@ import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import io.othree.aok.AsyncBaseTest
 
-abstract class AsyncAkkaTest (_system: ActorSystem) extends TestKit(_system) with AsyncBaseTest
+abstract class AsyncAkkaTest (system: ActorSystem) extends TestKit(system) with AsyncBaseTest
--- src/main/scala/io/othree/akkaok/BaseAkkaTest.scala
+++ src/main/scala/io/othree/akkaok/BaseAkkaTest.scala
@@ -4,4 +4,4 @@ import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import io.othree.aok.BaseTest
 
-abstract class BaseAkkaTest(_system: ActorSystem) extends TestKit(_system) with BaseTest
+abstract class BaseAkkaTest(system: ActorSystem) extends TestKit(system) with BaseTest
